e3d1c2d5a6fb51671e8933f2f214cd33e1354893
MADESH122 <madeshwaran9080@gmail.com>
2023-09-16 16:16:47 +0530

    changed

--- script.js
+++ script.js
@@ -15,9 +15,36 @@ function br(tagname){
 return res;
 }
 
+
+
+ 
+var Firstname=createlable("lable","for","Firstname","Firstname");
+var br1=br("br")
+var input=inputele("input","text","Firstname","id","Firstname")
+var br2=br("br")
+document.body.append(Firstname,br1,input,br2)
+var Middlename=createlable("lable","for","Middlename","Middlename");
+var br1=br("br")
+var input=inputele("input","text","Middlename","id","Middlename")
+var br2=br("br")
+document.body.append(Middlename,br1,input,br2)
+
+var Lastname=createlable("lable","for","Lastname","Lastname");
+var br1=br("br")
+var input=inputele("input","text","Middlename","id","Lastname")
+var br2=br("br")
+document.body.append(Lastname,br1,input,br2)
+
 var email=createlable("lable","for","email","Email");
 var br1=br("br")
 var input=inputele("input","text","email","id","email")
 var br2=br("br")
 
+
 document.body.append(email,br1,input,br2)
+ 
+var pin=createlable("lable","for","pin","pin");
+var br1=br("br")
+var input=inputele("input","text","pin","id","pin")
+var br2=br("br")
+document.body.append(pin,br1,input,br2)
